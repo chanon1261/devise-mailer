f91e0a47319072ab83867f72dbfa4d454112773e
M416 <chanon_w@cmu.ac.th>
2018-06-26 11:54:39 +0700

    create devise

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  #devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  authenticated :user do
+		root to: 'home#index'
+	end
+
+	unauthenticated :user do
+		devise_scope :user do
+			root to: 'devise/sessions#new'
+		end
+	end
+end
